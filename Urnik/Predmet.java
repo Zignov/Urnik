e1be7fb1b294d0d49cc70f48177272b9e96d8adb
Zignov <50624283+Zignov@users.noreply.github.com>
2023-03-30 23:34:21 +0200

    Add files via upload

---
+++ Urnik/Predmet.java
@@ -0,0 +1,24 @@
+package Urnik;
+
+public class Predmet {
+    
+    private String okrajsava;
+    private String ime;
+    private String opis;
+
+    Predmet predmet = new Predmet();
+
+
+    String vrniIme (String ime){
+        return(predmet.okrajsava);
+    }
+
+    String vrniOkrajsavo (String okrajsava){
+        return(predmet.okrajsava);
+    }
+
+    String vrniOpis (String opis){
+        return(predmet.opis);
+    }
+
+}
---
+++ Urnik/Ura.java
@@ -0,0 +1,12 @@
+package Urnik;
+
+public class Ura {
+    
+    for (int i = 0; i< steviloPredmetov; i++){
+        Predmet ime = new Predmet();
+    }
+
+    String cas;
+
+}
+
